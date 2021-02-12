b48d77d6c3872dd75e588aea74b04a5b84dc258d
Gus Teran <ateran@wpi.edu>
2021-02-11 23:42:57 -0500

    Quick Fixes
    Fix comment. Fix quotation marks

--- commands/love.js
+++ commands/love.js
@@ -1,13 +1,13 @@
-//simple 'wanna ball' command
+//love calulator using rapid api
 
 const unirest = require("unirest");
 const { rapid_api_key } = require("../config.json")
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 
 module.exports = (msg, tokens) => {
     if (tokens.length != 3) {
-        msg.channel.send("Your command is formatted improperly!\nPlease use 'love [name1] [name2]'");
+        msg.channel.send("Your command is formatted improperly!\nPlease use 'love (name) (name)'");
         return;
     }
 
@@ -28,14 +28,14 @@ module.exports = (msg, tokens) => {
     req.end(function (res) {
         if (res.error) throw new Error(res.error);
 
-        var heart_img = "https://ih1.redbubble.net/image.1328908604.1387/st,small,507x507-pad,600x600,f8f8f8.jpg";
+        let heart_img = "https://ih1.redbubble.net/image.1328908604.1387/st,small,507x507-pad,600x600,f8f8f8.jpg";
         if(parseInt(res.body.percentage) > 50){
-            heart_img = 'https://www.nicepng.com/png/detail/896-8968612_minecraft-heart-minecraft-health-bar-png.png';
+            heart_img = "https://www.nicepng.com/png/detail/896-8968612_minecraft-heart-minecraft-health-bar-png.png";
         }
 
         const loveRating = new MessageEmbed()
                 .setColor("FF69B4")
-                .setTitle(`Love Calulator for `+tokens[1]+' and '+tokens[2])
+                .setTitle("Love Calulator for "+tokens[1]+" and "+tokens[2])
                 .setThumbnail(heart_img)
                 .addFields(
                     { name: "Love Level", value: res.body.percentage, inline: true },
