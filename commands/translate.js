df0fdf1a8dff2747fbf2bbc7650779b387f364cf
PaskettiDesk <zxprospinsxz@gmail.com>
2021-02-12 14:59:26 -0500

    working !translate reply from en to es

--- commands/translate.js
+++ commands/translate.js
@@ -4,8 +4,18 @@ const { rapid_api_key } = require("../config.json");
 
 
 module.exports = (msg, tokens) => {
-    const req = unirest("POST", "https://google-translate1.p.rapidapi.com/language/translate/v2");
 
+    const openParenIndex = msg.content.search(/\(/g);
+    const closeParenIndex = msg.content.search(/\)/g);
+    if(!(openParenIndex + 1 < closeParenIndex)){
+        msg.channel.send("Message formatted improperly! Try again!");
+        return;
+    }
+
+    const sourceString = msg.content.substring(openParenIndex + 1, closeParenIndex);
+
+
+    const req = unirest("POST", "https://google-translate1.p.rapidapi.com/language/translate/v2");
     req.headers({
         "content-type": "application/x-www-form-urlencoded",
         "accept-encoding": "application/gzip",
@@ -15,14 +25,16 @@ module.exports = (msg, tokens) => {
     });
 
     req.form({
-        "q": "Hello, world!",
+        "q": sourceString,
         "source": "en",
         "target": "es"
     });
 
     req.end(function (res) {
         if (res.error) throw new Error(res.error);
-    
-        console.log(res.body);
+
+        const translatedText = res.body.data.translations[0].translatedText;
+
+        msg.channel.send(translatedText);
     });
 }
