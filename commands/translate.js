6ddea12a5c12ee68a52fe68beabb6b66a60e959a
PaskettiDesk <zxprospinsxz@gmail.com>
2021-02-12 15:13:54 -0500

    checks language support before query, comments

--- commands/translate.js
+++ commands/translate.js
@@ -1,17 +1,34 @@
 
 const unirest = require("unirest");
 const { rapid_api_key } = require("../config.json");
+const {languages} = require("../data.json");
 
 
 module.exports = (msg, tokens) => {
-
-    const openParenIndex = msg.content.search(/\(/g);
-    const closeParenIndex = msg.content.search(/\)/g);
-    if(!(openParenIndex + 1 < closeParenIndex)){
-        msg.channel.send("Message formatted improperly! Try again!");
+    //The msg string is going to need at least 4 tokens to be formatted properly.
+    if(tokens.length < 4){
+        msg.channel.send("Message formatted improperly! Try again! (use !help for guidance)");
         return;
     }
 
+    const languageFrom = tokens[1];
+    const languageTo = tokens[2];
+    //if varvbot doesn't recognize the given language(s), don't bother with an api call, tell the user about it.
+    if(!(languages.includes(languageTo) && languages.includes(languageFrom))){
+        msg.channel.send("One or more of the languages you've chosen is unsupported, try again!");
+        return;
+    }
+
+
+    const openParenIndex = msg.content.search(/\(/g);
+    const closeParenIndex = msg.content.search(/\)/g);
+    //open paren has to be left of the close paren.
+    if(!(openParenIndex + 1 < closeParenIndex)){
+        msg.channel.send("Message formatted improperly! Try again! (use !help for guidance)");
+        return;
+    }
+
+    //extract the string from between parens.
     const sourceString = msg.content.substring(openParenIndex + 1, closeParenIndex);
 
 
