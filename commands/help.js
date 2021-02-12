d0d3e3fa083883e50e9787b9ee82e4192227776a
PaskettiDesk <zxprospinsxz@gmail.com>
2021-02-12 15:38:30 -0500

    updated !help

--- commands/help.js
+++ commands/help.js
@@ -5,14 +5,15 @@ module.exports = (msg) => {
     let str = "";
     str += "```md\n";
     str += "+ Varvbot has numerous exciting features such as...\n";
-    str += '<ping               = "pong!">\n';
-    str += '<joke               = "enjoy one of VarvBot\'s numerous hilarious jokes!">\n';
-    str += '<roll n             = "rolls a dice with n sides!">\n';
-    str += '<ball               = "Wanna ball?">\n';
-    str += '<flip               = "Flip a coin!">\n';
-    str += '<love (name) (name) = "Find out your love score!">\n';
-    str += '<stock (symbol)     = "Gives information on the stock specified by the given symbol!">\n';
-    str += '<help               = "help!">\n';
+    str += '<ping                           = "pong!">\n';
+    str += '<joke                           = "enjoy one of VarvBot\'s numerous hilarious jokes!">\n';
+    str += '<roll n                         = "rolls a dice with n sides!">\n';
+    str += '<ball                           = "Wanna ball?">\n';
+    str += '<flip                           = "Flip a coin!">\n';
+    str += '<love (name) (name)             = "Find out your love score!">\n';
+    str += '<stock (symbol)                 = "Gives information on the stock specified by the given symbol!">\n';
+    str += '<translate (l1) (l2) (msg)      = "Translates a message from language 1 to language 2!">\n';
+    str += '<help                           = "help!">\n';
     str += "```";
     
     msg.channel.send(str);
