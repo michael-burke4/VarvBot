8d9f46f03302659d0a7d627ad033cdbebfdc1adf
PaskettiDesk <zxprospinsxz@gmail.com>
2021-02-19 00:11:22 -0500

    cleaned up !joke command

--- commands/joke.js
+++ commands/joke.js
@@ -1,9 +1,7 @@
-let fs = require("fs");
-let rawData = fs.readFileSync("data.json");
+let { jokes } = require("../data.json");
 
 //varvbot is simply too funny
 module.exports = (msg) => {
-    let data = JSON.parse(rawData);
-    let index = Math.floor(data.jokes.length * Math.random());
-    msg.channel.send(data.jokes[index]);
+    let index = Math.floor(jokes.length * Math.random());
+    msg.channel.send(jokes[index]);
 }
