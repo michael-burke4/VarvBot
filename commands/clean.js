619ea1636ebeb9213d13ab27ab61c8508c7fd275
PaskettiDesk <zxprospinsxz@gmail.com>
2021-02-18 20:58:02 -0500

    added admin_roles support in !clean

--- commands/clean.js
+++ commands/clean.js
@@ -1,20 +1,39 @@
-const config = require("../config.json");
-const adminRoles = config.admin_roles;
+const { admin_roles } = require("../config.json");
 
 module.exports = async (msg, tokens) => {
-    //currently only michael b is allowed to issue the !clean command, will eventually add support for any of the admin roles
-    //specified in config.json to use the command
-    if (msg.author.id == "213836206402699265") {
-        let numToDelete = parseInt(tokens[1]);
-
-        try {
-            let fetched = await msg.channel.messages.fetch({ limit: numToDelete + 1 });
-            msg.channel.bulkDelete(fetched);
-        } catch (error) {
-            console.log(error);
+    let allowed = false;
+    const rolesCollection = msg.member.roles.cache;
+    for (let role of rolesCollection.array()) {
+        if (admin_roles.includes(role.name)) {
+            allowed = true;
         }
     }
-    else {
+
+    // User permission/role check
+    if (!allowed) {
         msg.channel.send("You are not authorized to use this command!");
+        return;
     }
+
+    //!clean formatting check
+    if (tokens.length != 2) {
+        msg.channel.send("Improperly formatted !clean request!");
+        return;
+    }
+
+    //Positive number check
+    let numToDelete = parseInt(tokens[1]);
+    if (isNaN(numToDelete) || numToDelete < 0) {
+        msg.channel.send("Invalid removal amount!");
+        return;
+    }
+
+    try {
+        let fetched = await msg.channel.messages.fetch({ limit: numToDelete + 1 });
+        msg.channel.bulkDelete(fetched);
+    } catch (error) {
+        console.log(error);
+        msg.channel.send("Could not delete messages!");
+    }
+
 }
