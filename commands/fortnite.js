4edb80f82561c9f1236c12af9ef18d55d6374626
PaskettiDesk <zxprospinsxz@gmail.com>
2021-02-19 18:28:04 -0500

    cleaned up the !fortnite command, nicer style.

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -1,21 +1,19 @@
-const { Message } = require("discord.js");
-
+//Joins the voice channel the calling member is in and plays the fortnite.mp3 file.
 module.exports = async (msg, tokens, client) => {
-    if (msg.member.voice.channel) {
-        const connection = await msg.member.voice.channel.join();
-        const dispatcher = connection.play("fortnite.mp3");
 
-
-        dispatcher.on("finish", () => {
-            dispatcher.destroy();
-            connection.disconnect();
-        });
-
-        // Always remember to handle errors appropriately!
-        dispatcher.on("error", console.error);
-
-    }
-    else {
+    if (!(msg.member.voice.channel)) {
         msg.channel.send("You are not in a voice channel!");
+        return;
     }
+
+    const connection = await msg.member.voice.channel.join();
+    const dispatcher = connection.play("fortnite.mp3");
+
+
+    dispatcher.on("finish", () => {
+        dispatcher.destroy();
+        connection.disconnect();
+    });
+
+    dispatcher.on("error", console.error);
 }
