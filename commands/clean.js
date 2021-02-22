7270ff1ffe7df77cac1e51ce2598bac0ef2ad524
Michael Burke <michael_burke4@student.uml.edu>
2021-02-22 18:05:04 -0500

    refactored a for loop

--- commands/clean.js
+++ commands/clean.js
@@ -4,15 +4,8 @@
 const { admin_roles } = require("../config.json");
 
 module.exports = async (msg, tokens) => {
-    let allowed = false;
-    const rolesCollection = msg.member.roles.cache;
-    //turn the collection into an array, iterate over it searching to see if any role matches an admin_role.
-    for (let role of rolesCollection.array()) {
-        if (admin_roles.includes(role.name)) {
-            allowed = true;
-            break;
-        }
-    }
+    const rolesArray = msg.member.roles.cache.array();
+    const allowed = rolesArray.filter(role => admin_roles.includes(role.name)).length != 0;
 
     if (!allowed) {
         msg.channel.send("You are not authorized to use this command!");
@@ -24,14 +17,14 @@ module.exports = async (msg, tokens) => {
         return;
     }
 
-    let numToDelete = parseInt(tokens[1]);
+    const numToDelete = parseInt(tokens[1]);
     if (isNaN(numToDelete) || numToDelete < 0) {
         msg.channel.send("Invalid removal amount!");
         return;
     }
 
     try {
-        let fetched = await msg.channel.messages.fetch({ limit: numToDelete + 1 });
+        const fetched = await msg.channel.messages.fetch({ limit: numToDelete + 1 });
         msg.channel.bulkDelete(fetched);
     } catch (error) {
         console.log(error);
