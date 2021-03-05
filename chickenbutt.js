d8f93bc95d4c7246708000b6cd9e6a85241084f0
Michael Burke <michael_burke4@student.uml.edu>
2021-03-04 22:25:15 -0500

    switched backticks to double quotes

--- chickenbutt.js
+++ chickenbutt.js
@@ -1,6 +1,6 @@
 module.exports = (msg, client) => {
     const lowerCase = msg.content.toLowerCase();
     if (lowerCase.startsWith("what ")) {
-        msg.channel.send(`Chicken Butt!`);
+        msg.channel.send("Chicken Butt!");
     }
 }
