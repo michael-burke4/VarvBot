05187767bfdc0713ec95cb4101beab8c181d2d48
Michael Burke <michael_burke4@student.uml.edu>
2021-04-02 14:42:05 -0400

    at someone checks all online users now

--- atsomeone.js
+++ atsomeone.js
@@ -1,8 +1,9 @@
-module.exports = (msg) => {
-    const memberArray = msg.channel.guild.members.cache.array();
+module.exports = async (msg) => {
+    const members = await msg.guild.members.fetch();
+    const filteredMemberArray = members.array().filter(user => user.presence.status != "offline" && user.presence.status != "dnd");
     let replaced = msg.content;
-    while(replaced.includes("@someone")){
-        replaced = replaced.replace("@someone", `${memberArray[Math.floor(memberArray.length * Math.random())].user}`);
+    while (replaced.includes("@someone")) {
+        replaced = replaced.replace("@someone", `${filteredMemberArray[Math.floor(filteredMemberArray.length * Math.random())].user}`);
     }
     msg.channel.send(replaced);
 }
