7ca2661e308fa7225a96f4f5bdfbd0c97b2d4b66
Michael Burke <michael_burke4@student.uml.edu>
2021-05-25 19:44:42 -0400

    alphabetized the !help list

--- commands/help.js
+++ commands/help.js
@@ -5,16 +5,17 @@ module.exports = (msg) => {
     let str = "";
     str += "```md\n";
     str += "+ Varvbot has numerous exciting features such as...\n";
-    str += '<ping                           = "pong!">\n';
-    str += '<joke                           = "enjoy one of VarvBot\'s numerous hilarious jokes!">\n';
-    str += '<roll n                         = "rolls a dice with n sides!">\n';
     str += '<ball                           = "Wanna ball?">\n';
-    str += '<flip                           = "Flip a coin!">\n';
-    str += '<love (name) (name)             = "Find out your love score!">\n';
-    str += '<stock (symbol)                 = "Gives information on the stock specified by the given symbol!">\n';
     str += '<clean (x)                      = "Removes the past x messages from the text channel the command is called in!">\n';
+    str += '<crypto (coin)                  = "displays price information related to the specified coin!">\n'
+    str += '<flip                           = "Flip a coin!">\n';
+    str += '<fortnite                       = "epicly pwn someone in your voice channel with the default dance song!">\n';
     str += '<help                           = "help!">\n';
-    str += '<fortnite                       = "epicly pwn someone in your voice channel with the default dance song!>\n';
+    str += '<joke                           = "enjoy one of VarvBot\'s numerous hilarious jokes!">\n';
+    str += '<love (name) (name)             = "Find out your love score!">\n';
+    str += '<ping                           = "pong!">\n';
+    str += '<roll n                         = "rolls a dice with n sides!">\n';
+    str += '<stock (symbol)                 = "Gives information on the stock specified by the given symbol!">\n';
     str += "```";
 
     msg.channel.send(str);
