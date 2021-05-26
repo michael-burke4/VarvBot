3eeb885b236868ffb0d78cb4ab4389f8bc910fa6
Michael Burke <michael_burke4@student.uml.edu>
2021-05-25 22:07:13 -0400

    tells everyone to react once the vote is complete

--- emojivote.js
+++ emojivote.js
@@ -1,14 +1,21 @@
 module.exports = async (msg) => {
     msg.react("✅");
     msg.react("❌");
+    msg.channel.send("@everyone do your civic duty and vote on this submission!");  
     setTimeout( () => {
         let checks = msg.reactions.cache.find(emoji => emoji.emoji.name == '✅').count - 1; 
         let xs = msg.reactions.cache.find(emoji => emoji.emoji.name == '❌').count - 1;
-        if(xs + checks > 5 && checks > (2 / 3) * (xs + checks)){
+        let passed = xs + checks > 5 && checks > (2 / 3) * (xs + checks)
+        // if(){
+        if(checks > xs){
             msg.channel.send(`${msg.author}'s submission has passed!`)
+            msg.channel.send("@everyone React to THIS message with the emoji you'd like to see removed!");
+            setTimeout( () => {
+                msg.channel.send("The vote is complete!");
+            }, 86400000);
         }
         else{
             msg.channel.send(`${msg.author}'s submission has NOT passed!`)
         }
-    }, 5000);
+    }, 86400000);
 }
