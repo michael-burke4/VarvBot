16fd3736c0b36753814fedbc32b6e520d760d167
Michael Burke <michael_burke4@student.uml.edu>
2021-05-25 21:51:32 -0400

    basic vote counting after 5 seconds

--- emojivote.js
+++ emojivote.js
@@ -1,4 +1,14 @@
-module.exports = (msg) => {
+module.exports = async (msg) => {
     msg.react("✅");
     msg.react("❌");
+    setTimeout( () => {
+        let checks = msg.reactions.cache.find(emoji => emoji.emoji.name == '✅').count - 1; 
+        let xs = msg.reactions.cache.find(emoji => emoji.emoji.name == '❌').count - 1;
+        if(xs + checks > 5 && checks > (2 / 3) * (xs + checks)){
+            msg.channel.send(`${msg.author}'s submission has passed!`)
+        }
+        else{
+            msg.channel.send(`${msg.author}'s submission has NOT passed!`)
+        }
+    }, 5000);
 }
