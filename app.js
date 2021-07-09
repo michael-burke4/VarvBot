2ac791dd29dc4c430a46db317c5b4572ea7a17f0
Michael Burke <michael_burke4@student.uml.edu>
2021-07-08 21:59:41 -0400

    cleaned up app.js and finished handler refactor

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
 const interactionHandler = require("./interactionhandler.js");
-const imjoke = require("./imjoke.js");
-const chickenButt = require("./chickenbutt.js");
-const atSomeone = require("./atsomeone.js");
 require("dotenv").config();
 
 
@@ -15,19 +11,6 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-    
-    if(message.content.includes("@someone")){
-        atSomeone(message);
-    }
-    
-    if (config.options.enableImJoke) {
-        imjoke(message, client);
-    }
-
-    if(config.options.enableChickenButt){
-        chickenButt(message,client);
-    }
-    
     interactionHandler(message, client);
 });
 
