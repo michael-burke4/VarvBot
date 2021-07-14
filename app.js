043d7d43fdfa31b1a0467495f1d2d19c27e307be
Michael Burke <michael_burke4@student.uml.edu>
2021-07-14 19:12:47 -0400

    fully implemented the hardly know her joke

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const config = require("./config.json");
 const commandHandler = require("./commands.js");
 const imjoke = require("./imjoke.js");
 const chickenButt = require("./chickenbutt.js");
+const hardlyKnowHer = require("./ihardlyknowher.js");
 const atSomeone = require("./atsomeone.js");
+const ihardlyknowher = require("./ihardlyknowher.js");
 require("dotenv").config();
 
 
@@ -15,19 +17,24 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-    
-    if(message.content.includes("@someone")){
+
+    if (message.content.includes("@someone")) {
         atSomeone(message);
     }
-    
+
     if (config.options.enableImJoke) {
         imjoke(message, client);
     }
 
-    if(config.options.enableChickenButt){
-        chickenButt(message,client);
+    if (config.options.enableChickenButt) {
+        chickenButt(message, client);
+    }
+
+    if(!message.author.bot ) {
+        ihardlyknowher(message, client);
     }
     
+
     commandHandler(message, client);
 });
 
--- ihardlyknowher.js
+++ ihardlyknowher.js
@@ -1,25 +1,14 @@
+function getLastWordStripped(string) {
+    let endString = '';
+    let lastWordArray = string.substring(string.lastIndexOf(' ') + 1);
+    lastWordArray.match(/\w+/g).forEach(str => endString += str);
+    return endString;
+}
+
 module.exports = (msg, client) => {
-    
-    const lowerCase = msg.content.toLowerCase();
-    var StartIndex;
-    var EndIndex;
-    var currentWord;
-
-    for (let i = 0; i < lowerCase.length; i++) {
-
-        if(lowerCase[i] == ' '){
-            //new word start gets updated to after the space
-            StartIndex = i + 1;
-
-            //end of the word is the next space or end of the message after the word start
-            EndIndex = lowerCase.indexOf(' ',StartIndex);
-
-            //gets the word from the start to the end
-            currentWord = lowerCase.substring(StartIndex,EndIndex);
-        }
-
-        if(lowerCase[i] == 'e' && lowerCase[i+1] == 'r'){
-            msg.channel.send(currentWord + "? I hardly know her!");
-        }
+    const lastWord = getLastWordStripped(msg.content);
+    if (lastWord.endsWith("er")) {
+        msg.channel.send(`${lastWord.substring(0, lastWord.length - 2)} her? I hardly know her!`);
     }
 }
+
