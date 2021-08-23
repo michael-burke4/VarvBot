30259863050bd8dd14a8c15f5d42786a249ea3b7
Michael Burke <michael_burke4@student.uml.edu>
2021-08-23 14:00:41 -0400

    fail safe for 0 logged messages

--- commands/ratio.js
+++ commands/ratio.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-module.exports = (msg) => {
+module.exports = (msg, tokens, client) => {
     fs.readFile("data.json", "utf-8", (err, rawData) => {
         if (err) {
             console.log(err);
@@ -10,7 +10,11 @@ module.exports = (msg) => {
         data = JSON.parse(rawData);
         const date = new Date();
         const dateStamp = `${date.getMonth()}m${date.getFullYear()}y`;
-        msg.channel.send(`${data.messages[dateStamp].bot} of the last ${data.messages[dateStamp].bot + data.messages[dateStamp].users} messages this month have been sent by VarvBot!`);
+        if(data.messages[dateStamp] == null) {
+            msg.channel.send("Message data unavailable for this month");
+            return;
+        }
+        msg.channel.send(`${data.messages[dateStamp].bot} of the last ${data.messages[dateStamp].bot + data.messages[dateStamp].users} messages this month have been sent by ${client.user.username}!`);
 
     });
 }
