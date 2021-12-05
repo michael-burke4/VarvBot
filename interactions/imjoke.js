eaff35028e35e258ed6672a81bc53883d3d179db
Michael Burke <michael_burke4@student.uml.edu>
2021-12-05 16:07:16 -0500

    regex refactor

--- interactions/imjoke.js
+++ interactions/imjoke.js
@@ -3,6 +3,6 @@ const config = require("../config.json");
 //if the message starts with some variant of I am, followed by some string "...", varvbot will respond by saying
 // Hi "...", I'm VarvBot!
 module.exports = (msg, client) => {
-    const jokeString = (/^(?:(?:i|I)(?:'?|’?)(?:m|M)|(?:(?:I|i) (?:A|a)(?:M|m)))\s(.+)$/g).exec(msg.content)?.[1];
+    const jokeString = (/^(?:I|i)(?: (?:a|A)(?:M|m)|(?:M|m|'m|'M|’m|’M)) +(.+)$/g).exec(msg.content)?.[1];
     jokeString != null ? msg.channel.send(`Hi ${jokeString}, I'm VarvBot!`) : 0;
 }
