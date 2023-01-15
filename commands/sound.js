8df83977681b0601b06cca67d97ab01afe4c311d
Michael Burke <michael_burke4@student.uml.edu>
2023-01-15 16:12:00 -0500

    bug finding + message data restructure (#17)
    * updated all node package versions
    
    some small changes in discord.js api, likely have to fix more.
    
    * fixed messagecounter not working w/ empty messagedata.json file
    
    * messagecounter refactor + final bugfixes
    
    * provide a template for messagedata.json
    
    * restructured message counter + found some bugs

--- commands/sound.js
+++ commands/sound.js
@@ -3,6 +3,7 @@ module.exports = {
     params : ["name"],
     description: "Plays the specified sound in your current voice channel!",
     fun: async (msg, tokens, client) => {
+        // FIXME: bot can't detect that the user is in a voice channel?
         if (!(msg.member.voice.channel)) {
             msg.channel.send("You are not in a voice channel!");
             return;
--- interactions/messagecounter.js
+++ interactions/messagecounter.js
@@ -16,20 +16,42 @@ module.exports = async (msg, client) => {
         const date = new Date();
         const dateStamp = `${date.getMonth()}m${date.getFullYear()}y`;
         if (data.lifetime == null) {
-            data.lifetime = {bot: 0, users: 0};
+            data.lifetime = {};
         }
+        if (data.lifetime[msg.author.id] == null) {
+            data.lifetime[msg.author.id] = {
+                username: msg.author.username,
+                messages: 0,
+            };
+        }
+        if (data.lifetime[msg.author.id].username != msg.author.username) {
+            data.lifetime[msg.author.id].username = msg.author.username;
 
-        let fieldToUpdate = (msg.author.id == client.user.id) ? "bot" : "users";
+        }
+        data.lifetime[msg.author.id].messages += 1;
+
         if (data[dateStamp] == null) {
-            data[dateStamp] = {bot: 0, users: 0};
+            data[dateStamp] = {};
         }
-        data[dateStamp][fieldToUpdate] += 1;
-        data["lifetime"][fieldToUpdate] += 1;
-        fs.writeFile("messagedata.json", JSON.stringify(data, null, 4), err => {
-            if (err) {
-                console.log(err);
-            }
-        });
+        if (data[dateStamp][msg.author.id] == null) {
+            data[dateStamp][msg.author.id] = {
+                username: msg.author.username,
+                messages: 0,
+            };
+        }
+        if (data[dateStamp][msg.author.id].username != msg.author.username) {
+            data[dateStamp][msg.author.id].username = msg.author.username;
+        }
+        data[dateStamp][msg.author.id].messages += 1;
 
+        fs.writeFile(
+            "messagedata.json",
+            JSON.stringify(data, null, 4),
+            (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            }
+        );
     });
-}
+};
