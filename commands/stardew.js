d2c17ebc52bfa9da2c9aa167fa54d370ba5e4910
Gus Teran <ateran@wpi.edu>
2023-02-27 09:39:27 -0500

    Add checking if the stardew server is up

--- commands/stardew.js
+++ commands/stardew.js
@@ -5,6 +5,22 @@ module.exports = {
     fun: (msg, tokens, client) => {
 
         let stardew = process.env.STARDEW_PATH;
+        let serverIsUp = false;
+        let appID = "413150";
+
+        exec('ps aux | grep Stardew',
+        function (error, stdout, stderr) {
+            if (error !== null) {
+                console.log('exec error: ' + error);
+                return;
+            }
+            serverIsUp = stdout.includs(appId);
+        });
+
+        if(!serverIsUp){
+            msg.channel.send(`The Stardew Valley server is currently down`);
+            return;
+        }
 
         fs.readFile(stardew, "utf-8", (err, rawData) => {
             if (err) {
@@ -17,8 +33,10 @@ module.exports = {
                 msg.channel.send("Invite code unavailable!");
                 return;
             }
-            msg.channel.send(`Join the BGC Stardew Server with invite code: ${invite}`);
-
+            msg.channel.send(`The server is up! Join the BGC Stardew server with invite code: ${invite}`);
+            return;
         });
+
+        msg.channel.send('Uh oh, something went wrong');
     }
 }
