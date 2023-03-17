b78de270aef7cbb03b7aa63490f9117893838ad4
Gus Teran <ateran@wpi.edu>
2023-03-16 21:54:56 -0400

    Voice progress
    Co-authored-by: Michael Burke <michael-burke4@users.noreply.github.com>

--- commands/dc.js
+++ commands/dc.js
@@ -1,9 +1,12 @@
+const sound = require('./sound.js');
+
 module.exports = {
     description: "Disconnects the bot from the current voice channel!",
     fun: async (msg, tokens, client) => {
-        client.dispatcher.destroy();
-        client.connection.disconnect();
-		client.dispatcher = null;
-		client.connection = null;
+        // client.dispatcher.destroy();
+        // client.connection.disconnect();
+		// client.dispatcher = null;
+		// client.connection = null;
+        sound.dc();
     }
 }
--- commands/sound.js
+++ commands/sound.js
@@ -1,3 +1,16 @@
+const { createReadStream } = require('node:fs');
+const { NoSubscriberBehavior, joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, StreamType, VoiceConnectionStatus } = require('@discordjs/voice');
+let connection = null;
+const player = createAudioPlayer({
+	behaviors: {
+		noSubscriber: NoSubscriberBehavior.Play,
+	},
+});
+let vc = null;
+
+player.on(AudioPlayerStatus.Playing, () => {
+	console.log('The audio player has started playing!');
+});
 
 module.exports = {
     params : ["name"],
@@ -12,12 +25,53 @@ module.exports = {
         if (tokens.length != 2) {
             return;
         }
-		if(client.connection == null) {
-        	client.connection = await msg.member.voice.channel.join();
+		if(connection == null) {
+        	// client.connection = await msg.member.voice.channel.join();
+            console.log("connection was null");
+            vc = msg.member.voice.channel;
+            connection = joinVoiceChannel({
+                channelId: vc.id,
+                guildId: vc.guild.id,
+                adapterCreator: vc.guild.voiceAdapterCreator,
+            });
 		}
 
-        client.dispatcher = client.connection.play(`sounds/${tokens[1]}.ogg`);
-        client.dispatcher.on("error", console.error);
+        const resource = createAudioResource(createReadStream(`sounds/${tokens[1]}.ogg`), {
+            inputType: StreamType.OggOpus,
+        });
+        player.play(resource);
+        connection.subscribe(player);
+
+
+        // client.dispatcher = client.connection.play(`sounds/${tokens[1]}.ogg`);
+        // client.dispatcher.on("error", console.error);
+    },
+    dc: async () => {
+        console.log("Disconnected from voice channel!");
+        player.stop();
+
+        // if(vc == null) {
+        //     console.log("No voice channel!");
+        //     return;
+        // }
+
+        if(connection == null) {
+            console.log("connection was null");
+            return;
+        }
+
+        connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+            try {
+                await Promise.race([
+                    entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+                    entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+                ]);
+                // Seems to be reconnecting to a new channel - ignore disconnect
+            } catch (error) {
+                // Seems to be a real disconnect which SHOULDN'T be recovered from
+                connection.destroy();
+            }
+        });
     }
 }
 
